a10dc1c4d694db007c8b0e167bdb5cff257144ee
DadiSoumia <163993573+DadiSoumia@users.noreply.github.com>
2026-07-02 03:28:35 +0200

    d

--- portfolio/src/pages/ProjectDetail.jsx
+++ portfolio/src/pages/ProjectDetail.jsx
@@ -1,7 +1,7 @@
 import { useParams, Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Footer from '../components/footer'
 
 export default function ProjectDetail() {
   const { id } = useParams()
